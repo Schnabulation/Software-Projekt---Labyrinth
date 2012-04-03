a793af56a8a1fb0dda3f2e48b1d82a551ab69a78
Reto Huber <huberre1@students.zhaw.ch>
2012-04-03 11:39:41 +0200

    dummy aenderig - GIT Test

--- src/dummyPackage/dummyClass.java
+++ src/dummyPackage/dummyClass.java
@@ -5,7 +5,7 @@ public class dummyClass {
 	private String s;
 	
 	public dummyClass() {
-		s = "Kei Ahnig, wie de Scheiss funktioniert";
+		s = "Kei Ahnig, wie de Scheiss funktioniert. - ICH AU N÷÷÷D. Hauptsach git gaht jetzt mal :)";
 		System.out.println(s);
 	}
 	
