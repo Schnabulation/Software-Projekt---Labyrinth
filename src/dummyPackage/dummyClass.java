38b6c976aeb1b1c698723303e6fce5db19ea9a2a
Steve Heller <steve.heller@schnabulation.ch>
2012-04-03 10:52:35 +0200

    Projekt initial erstellen, GitHub Repo erstellen

---
+++ src/dummyPackage/dummyClass.java
@@ -0,0 +1,16 @@
+package dummyPackage;
+
+public class dummyClass {
+	
+	private String s;
+	
+	public dummyClass() {
+		s = "Kei Ahnig, wie de Scheiss funktioniert";
+		System.out.println(s);
+	}
+	
+	public static void main(String[] args) {
+		dummyClass dc = new dummyClass();
+	}
+
+}
