9043438bdf2db21b8bbeb4ba845c9c9e5b7510cf
Steve Heller <steve.heller@schnabulation.ch>
2012-04-15 13:27:51 +0200

    Leere Klasse fuer den A* Algorithmus hinzugefuegt

---
+++ src/algorithmus/A_star.java
@@ -0,0 +1,12 @@
+package algorithmus;
+
+import labyrinth.Labyrinth;
+
+public class A_star extends Algorithmus {
+
+	@Override
+	public Labyrinth solveLab(Labyrinth originalLab) {
+		return originalLab;
+	}
+
+}
